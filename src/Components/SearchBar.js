1c944962a50f1d0b782b0e5b5b7c47d43d8139f5
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-09-23 10:52:22 +0100

    last check

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = () => {
     const {handleChange, searchValue} = useContext(GlobalContext)
   return (
     <div className='search-bar'>
-        <input type='search' value={searchValue} onChange={handleChange} id='search' name='search' placeholder='Enter a tag name to search'/>
+        <input type='search' value={searchValue || ""} onChange={handleChange} id='search' name='search' placeholder='Enter a tag name to search'/>
         <img src={lens} alt='lens'/>
     </div>
   )
